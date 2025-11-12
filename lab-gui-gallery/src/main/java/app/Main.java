236bb296ab4eb0bc09d6447a3258e49511f87e83
PawoLin <czajczykp@gmail.com>
2025-11-12 08:23:27 +0100

    init lab-gui

---
+++ lab-gui-gallery/src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static final String GOOGLE_CUSTOM_SEARCH_API_KEY = "*** Enter your https://www.scraperapi.com API key HERE ***";
+
+
+    public static void main(String[] args) {
+        Application.launch(GalleryApp.class);
+    }
+}
