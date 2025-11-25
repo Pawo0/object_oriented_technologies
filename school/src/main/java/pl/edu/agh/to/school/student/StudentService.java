5a2912b14eb0d5599ba883601d2bf614c66be990
PawoLin <czajczykp@gmail.com>
2025-11-25 08:36:54 +0100

    pierwsza usulga REST i kontroller do studenta

--- school/src/main/java/pl/edu/agh/to/school/student/StudentService.java
+++ school/src/main/java/pl/edu/agh/to/school/student/StudentService.java
@@ -2,9 +2,16 @@ package pl.edu.agh.to.school.student;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    public List<Student> getStudents(){
+        return List.of(
+                new Student("Jan", "Kowalski", LocalDate.of(2000, 1, 15), "123456"),
+                new Student("Anna", "Nowak", LocalDate.of(1999, 5, 30), "654321")
+        );
+    }
 }
